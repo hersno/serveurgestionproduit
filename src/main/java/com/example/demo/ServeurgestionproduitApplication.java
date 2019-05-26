9c763d807339406ac1a2b8105ce24a1b7e6707be
hersno <herisoanoe@gmail.com>
2019-05-26 16:13:13 +0200

    test
    git commit -m test
    git commit -m test

---
+++ src/main/java/com/example/demo/ServeurgestionproduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurgestionproduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurgestionproduitApplication.class, args);
+	}
+
+}
